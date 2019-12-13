4702a7facd18de0cdf84b9bf104a805b0dbf8992
Tristanlaf003 <55155457+Tristanlaf003@users.noreply.github.com>
2019-12-13 09:14:26 -0500

    ExamenFinal20

--- DesignWeb/ExamenFinal/js/examenFinal.js
+++ DesignWeb/ExamenFinal/js/examenFinal.js
@@ -19,4 +19,26 @@ $(document).ready(function () {
     AOS.init({duration : 1200});
 });
 
+let click = 1;
+let imageID1;
+let imageID2;
+let imageSrc1;
+let imageSrc2;
+
+$('.change').click(function () {
+    if (click === 1){
+        imageID1 = $(this).attr('id');
+        imageSrc1 = $(this).attr('src');
+        click = 2;
+
+    }
+    else if (click === 2){
+        imageID2 = $(this).attr('id');
+        imageSrc2 = $(this).attr('src');
+
+        $('#' + imageID1).attr('src', imageSrc2);
+        $('#' + imageID2).attr('src', imageSrc1);
+        click = 1;
+    }
+});
 
