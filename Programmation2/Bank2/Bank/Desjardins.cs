65eb608757f94f19a8c265db5337ee3742b214a2
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-17 11:36:41 -0400

    AjouterSuccursale

--- Programmation2/Bank2/Bank/Desjardins.cs
+++ Programmation2/Bank2/Bank/Desjardins.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bank
 {
     public class Desjardins
     {
-        
+        private string nomBank = "Desjardins";
+        private string numroInstitution = "815";
+        private List<Succursale> lesSuccursale = new List<Succursale>();
+
+        public int AjouterSuccursale(Succursale leSuccursale)
+        {
+            lesSuccursale.Add(leSuccursale);
+            return lesSuccursale.Count;
+        }
     }
 }
