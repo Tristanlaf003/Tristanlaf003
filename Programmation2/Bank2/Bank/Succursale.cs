aa8d8e587e2cc0e9039793ae0558a0b606e1dc9b
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-19 17:24:58 -0400

    AjouterClients

--- Programmation2/Bank2/Bank/Succursale.cs
+++ Programmation2/Bank2/Bank/Succursale.cs
@@ -6,5 +6,20 @@ namespace Bank
 {
     public class Succursale
     {
+        private List<Clients> lesClients = new List<Clients>();
+        private string prenom;
+        private string nom;
+        private uint numeroIdentification;
+
+        /// <summary>
+        /// Permet d'ajouter un clients dans une succursale
+        /// </summary>
+        /// <param name="leClient"></param>
+        /// <returns>Retourne le nombre de clients dans la succursale</returns>
+        public int AjouterClients(Clients leClient)
+        {
+            lesClients.Add(leClient);
+            return lesClients.Count;
+        }
     }
 }
