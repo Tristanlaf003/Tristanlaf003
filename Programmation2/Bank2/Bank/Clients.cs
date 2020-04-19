7594086e2e0a3a5f6eff57df41490dca3923d3d0
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-19 17:45:56 -0400

    AjouterCompte

--- Programmation2/Bank2/Bank/Clients.cs
+++ Programmation2/Bank2/Bank/Clients.cs
@@ -6,6 +6,20 @@ namespace Bank
 {
     public class Clients
     {
+        private uint numeroCompte;
+        private ushort chiffreVerificateur;
+        private List<Compte> lesComptes = new List<Compte>();
+
+        /// <summary>
+        /// Permet d'ajouter un compte à un client
+        /// </summary>
+        /// <param name="leCompte"></param>
+        /// <returns>Nombre de compte que possède un client</returns>
+        public int AjouterCompte(Compte leCompte)
+        {
+            lesComptes.Add(leCompte);
+            return lesComptes.Count;
+        }
 
     }
 }
