598638815c69e2360ca548aaae45df47ccb5ca51
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-19 20:48:25 -0400

    ModificationVariable

--- Programmation2/Bank2/Bank/Compte.cs
+++ Programmation2/Bank2/Bank/Compte.cs
@@ -6,6 +6,20 @@ namespace Bank
 {
     public class Compte
     {
+        private uint numeroCompte;
+        private ushort chiffreVerificateur;
         private int montantArgent;
+
+        public Compte(uint numeroCompte, ushort chiffreVerificateur, int montantArgent)
+        {
+            this.NumeroCompte = numeroCompte;
+            this.ChiffreVerificateur = chiffreVerificateur;
+            this.MontantArgent1 = montantArgent;
+        }
+
+        public int MontantArgent { get => MontantArgent1; set => MontantArgent1 = value; }
+        public uint NumeroCompte { get => numeroCompte; set => numeroCompte = value; }
+        public ushort ChiffreVerificateur { get => chiffreVerificateur; set => chiffreVerificateur = value; }
+        public int MontantArgent1 { get => montantArgent; set => montantArgent = value; }
     }
 }
