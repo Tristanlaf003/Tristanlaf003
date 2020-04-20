38d23b9eedf115e6c3b51616fa9fc7a5ab28b28f
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-19 20:52:05 -0400

    AffichageBankProgram

---
+++ Programmation2/Bank2/AffichageBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+
+namespace AffichageBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Desjardins bank = GenerationDesSuccursale();
+            
+            Console.WriteLine("Entrez le nom de la caisse");
+            Clients clients = new Clients("Tristan", "Lafontaine");
+            bank.NomSuccursale = Console.ReadLine();
+            bank.AjouterClient(clients);
+            Console.WriteLine(bank.ToString());
+        }
+
+        private static Desjardins GenerationDesSuccursale()
+        {
+            const int maxSuccursale = 3;
+            string[] nomSuccursale = new string[maxSuccursale] { "Victoriaville", "Montréal", "Québec" };
+
+            Random rnd = new Random();
+            Desjardins bank = new Desjardins();
+
+            for (int compteurSuccursale = 0; compteurSuccursale < maxSuccursale; compteurSuccursale++)
+            {
+                int randomIdentification = rnd.Next(10000, 99999) + 1;
+
+                Succursale nouvelleSuccursale = new Succursale(nomSuccursale[compteurSuccursale], randomIdentification);
+                bank.AjouterSuccursale(nouvelleSuccursale);
+            }
+            return bank;
+        }
+    }
+}
