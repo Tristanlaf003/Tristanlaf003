a359054b9ed1026e16203e2b45232c3184aa92e7
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-20 23:23:37 -0400

    SupprimerCompte

--- Programmation2/Bank2/Bank/Succursale.cs
+++ Programmation2/Bank2/Bank/Succursale.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Bank
 {
-    public class Succursale
+    public class Succursale : IEquatable<Succursale>
     {
         private List<Clients> lesClients = new List<Clients>();
         private int numeroIdentification;
@@ -33,17 +33,77 @@ namespace Bank
             return lesClients.Count;
         }
 
+        /// <summary>
+        /// Permet d'ajouter un compte à un client
+        /// </summary>
+        /// <param name="compte"></param>
+        /// <returns>Vrai si il a ajouter un compte sinon faux</returns>
         public bool AjouterCompte(Compte compte)
         {
             foreach (Clients clients in lesClients)
-            {
-                if (clients.PrenomClient == clients.PrenomClient)
+            { 
+                if (clients.NomClient.Equals(clients.NomClient))
                 {
                     clients.AjouterCompte(compte);
                     return true;
                 }
             }
             return false;
+        }
+
+        /// <summary>
+        /// Permet de supprimer un compte d'un client
+        /// </summary>
+        /// <param name="compte"></param>
+        /// <returns>Vrai si le compte est supprimer et faux si le compte n'est pas supprimer</returns>
+        public bool SupprimerCompte(Compte compte)
+        {
+            foreach (Clients clients in lesClients)
+            {
+                if (clients.NomClient.Equals(clients.NomClient))
+                {
+                    clients.SupprimerCompte(compte);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Permet de vérifier si c'est bien un objet qui est entrain de comparaitre
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>False si ce n'est pas un objet et si ce n'est pas une succursale et Vrai si les deux conditions sont vrai</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is Succursale))
+            {
+                return false;
+            }
+
+            return Equals(obj as Succursale);
+        }
+
+        /// <summary>
+        /// Equals avec une autre succursale
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>Vrai si les succursales sont équivalente sur le nom de la succursale</returns>
+        public bool Equals(Succursale other)
+        {
+            return other != null &&
+                NomSuccursale == other.NomSuccursale;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 364868598;
+            hashCode = hashCode * -1521134295 + NumeroIdentification.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NomSuccursale);
+            return hashCode;
         }
 
         public override string ToString()
