8109c90efc602ae26a56bc2a9cfc7427f210c8e8
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-20 23:22:07 -0400

    AjouterCompte

--- Programmation2/Bank2/Bank/Clients.cs
+++ Programmation2/Bank2/Bank/Clients.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Bank
 {
-    public class Clients
+    public class Clients : IEquatable<Clients>
     {
         private string prenomClient;
         private string nomClient;
@@ -28,6 +28,64 @@ namespace Bank
         {
             lesComptes.Add(leCompte);
             return lesComptes.Count;
+        }
+
+        
+
+        /// <summary>
+        /// Permet de supprimer un compte d'un client
+        /// </summary>
+        /// <param name="compte"></param>
+        /// <returns>Vrai si le compte est supprimer et faux si le compte n'est pas supprimer</returns>
+        public bool SupprimerCompte(Compte compte)
+        {
+            foreach (Compte compte1 in lesComptes)
+            {
+                if (compte1.Equals(compte))
+                {
+                    lesComptes.Remove(compte);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Permet de vérifier si c'est bien un ojet qui n'est pas null et que c'est un objet de type Client
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Vrai si l'oject n'est pas null et qui est de type clients</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Clients))
+            {
+                return false;
+            }
+            return Equals(obj as Clients);
+        }
+
+        /// <summary>
+        /// Equals avec un client comme autre élément
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>Retourne vrai si les clients sont égaux sur le prénom et le nom </returns>
+        public bool Equals(Clients other)
+        {
+            return other != null &&
+                PrenomClient == other.PrenomClient &&
+                NomClient == other.NomClient;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1232589645;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(PrenomClient);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NomClient);
+            return hashCode;
         }
     }
 }
