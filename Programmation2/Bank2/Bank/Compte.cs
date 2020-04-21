da6d2cdef53ed4258a35cdee467fc4ea1d3acbfd
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-20 23:22:37 -0400

    SupprimerCompte

--- Programmation2/Bank2/Bank/Compte.cs
+++ Programmation2/Bank2/Bank/Compte.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Bank
 {
-    public class Compte
+    public class Compte : IEquatable<Compte>
     {
         private uint numeroCompte;
         private ushort chiffreVerificateur;
@@ -21,5 +21,43 @@ namespace Bank
         public uint NumeroCompte { get => numeroCompte; set => numeroCompte = value; }
         public ushort ChiffreVerificateur { get => chiffreVerificateur; set => chiffreVerificateur = value; }
         public int MontantArgent1 { get => montantArgent; set => montantArgent = value; }
+
+        /// <summary>
+        /// Permet de vérifier si c'est bien un ojet qui n'est pas null et que c'est un objet de type Compte
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Vrai si l'oject n'est pas null et qui est de type compte</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Compte))
+            {
+                return false;
+            }
+            return Equals(obj as Compte);
+        }
+
+        /// <summary>
+        /// Equals avec un compte comme autre élément
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>Vrai si les comptes sont équaux sur le numéro de compte et le chiffre vérificateur</returns>
+        public bool Equals(Compte other)
+        {
+            return other != null &&
+                NumeroCompte == other.NumeroCompte &&
+                ChiffreVerificateur == other.ChiffreVerificateur;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 315562398;
+            hashCode = hashCode * -1521134295 + NumeroCompte.GetHashCode();
+            hashCode = hashCode * -1521134295 + ChiffreVerificateur.GetHashCode();
+            return hashCode;
+        }
     }
 }
