2be8e09da8628e6cd813cbc3633bd9487d3aeb49
Tristanlaf003 <lafontaine.tristan@carrefour.cegepvicto.ca>
2020-04-21 00:29:55 -0400

    RetraitArgent

--- Programmation2/Bank2/Bank/Desjardins.cs
+++ Programmation2/Bank2/Bank/Desjardins.cs
@@ -78,6 +78,14 @@ namespace Bank
             return false;
         }
 
+        /// <summary>
+        /// Permet de de faire un dépôt d'argent
+        /// </summary>
+        /// <param name="montant">Int</param>
+        /// <param name="clients">Prénom et nom</param>
+        /// <param name="compte">Numéro du compte, chiffre vérificateur et montant d'argent</param>
+        /// <param name="succursale">Numéro d'identification et le nom de la succursale</param>
+        /// <returns>Vrai si le dépôt est réussi et faux si le dépôt n'est pas réussi</returns>
         public bool DepotArgent(int montant, Clients clients, Compte compte, Succursale succursale)
         {
             foreach (Succursale succursale1 in lesSuccursale)
@@ -85,6 +93,27 @@ namespace Bank
                 if (succursale1.Equals(succursale))
                 {
                     succursale1.DepotArgent(montant, compte, clients);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Permet defaire un retrait d'argent
+        /// </summary>
+        /// <param name="montant"></param>
+        /// <param name="clients"></param>
+        /// <param name="compte"></param>
+        /// <param name="succursale"></param>
+        /// <returns>Vrai si le retrait d'argent a fonctionné et faux si le retrait d'argent n'a pas fonctionné</returns>
+        public bool RetraitArgent(int montant, Clients clients, Compte compte, Succursale succursale)
+        {
+            foreach (Succursale succursale1 in lesSuccursale)
+            {
+                if (succursale1.Equals(succursale))
+                {
+                    succursale1.RetraitArgent(montant, compte, clients);
                     return true;
                 }
             }
